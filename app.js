73ba93b4fc3d05bb082614755e1ced67b29cc6e8
Edwin ANDRIVET <edwinandrivet@users.noreply.github.com>
2017-10-10 16:07:38 +0200

    Fixed nuxt.js and nuxt.js as koa middleware. Updated semver nuxt.js broke the app : - Importing nuxt.js changed a bit, deconstructor needed. - See issue #1206 for the koa middleware integration.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const koaBody = require('koa-bodyparser');
 const cors = require('kcors');
 const mongoose = require('koa-mongoose');
 const json = require('koa-json');
-const Nuxt = require('nuxt');
+const { Builder, Nuxt } = require('nuxt');
 
 const config = require('./nuxt.config.js');
 const genericCRUD = require('./server/generic.js');
@@ -12,13 +12,12 @@ const genericCRUD = require('./server/generic.js');
 const app = new Koa();
 app.use(cors());
 const router = new KoaRouter();
-config.dev = !(app.env === 'production');
-
 const nuxt = new Nuxt(config);
 
 // Build only in dev mode
-if (config.dev) {
-  nuxt.build()
+if (nuxt.options.dev) {
+  new Builder(nuxt)
+  .build()
   .catch((error) => {
     console.error(error); // eslint-disable-line no-console
     process.exit(1);
@@ -48,9 +47,18 @@ router.use('/api/scores', genericCRUD('Score').routes());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.use(async (ctx) => {
+app.use((ctx) => {
   ctx.status = 200; // koa defaults to 404 when it sees that status is unset
-  await nuxt.render(ctx.req, ctx.res);
+
+  // Solves nuxt.js issue #1206 
+  return new Promise((resolve, reject) => {
+    ctx.res.on('close', resolve);
+    ctx.res.on('finish', resolve);
+    nuxt.render(ctx.req, ctx.res, (promise) => {
+      // nuxt.render passes a rejected promise into callback on error.
+      promise.then(resolve).catch(reject);
+    });
+  });
 });
 
 app.listen(3000);
